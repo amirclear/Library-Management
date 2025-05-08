8bf4c27f30b0214ca2eaa3bab79714f65bfad8cd
Amir <amirhoseinpanjeh878@gmail.com>
2025-05-08 22:57:36 +0330

    add queue package method

--- src/datastructures/queues/CustomQueue.java
+++ src/datastructures/queues/CustomQueue.java
@@ -3,6 +3,8 @@ package datastructures.queues;
 import datastructures.interfaces.Queue;
 import datastructures.lists.CustomLinkedList;
 
+import java.util.NoSuchElementException;
+
 public class CustomQueue<T> implements Queue<T> {
     private CustomLinkedList<T> list;
 
@@ -10,6 +12,54 @@ public class CustomQueue<T> implements Queue<T> {
         list = new CustomLinkedList<>();
     }
 
-    // TODO: Override and fill the methods to complete the data structure
+    @Override
+    public boolean add(Object t) throws IllegalStateException {
+        if (list.add(t)) return true;
+        throw new IllegalStateException();
+    }
+
+    @Override
+    public boolean offer(Object t) {
+        return list.add(t);
+    }
+
+    @Override
+    public T remove() throws NoSuchElementException {
+        if (list.isEmpty()) throw new NoSuchElementException();
+        return list.removeFirst();
+    }
+
+    @Override
+    public T poll() {
+        if (list.isEmpty()) return null;
+        return list.removeFirst();
+    }
+
+    @Override
+    public T element() throws NoSuchElementException {
+        if (list.isEmpty()) throw new NoSuchElementException();
+        return list.getFirst();
+    }
+
+    @Override
+    public T peek() {
+        if (list.isEmpty()) return null;
+        return list.getFirst();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    public void display() {
+        list.display();
+    }
+
 
 }
